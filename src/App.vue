8e1801d3541703a85d6d1c9fb573072d85944d24
Radu Popescu <rp@f6s.com>
2018-08-02 16:46:45 +0300

    Added timeline request

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <template>
-  <div id="app">
-    <git-table></git-table>
+    <div id="app">
+        <div>
+            Username:
+            <input ref="usernameInput"/>
+            <button @click="setUsername">Set</button>
+        </div>
+        <div>
+            Token:
+            <input ref="tokenInput"/>
+            <button @click="setToken">Set</button>
+        </div>
+
+        <git-table
+            v-if="username && token"
+            :username="username"
+            :token="token"
+        ></git-table>
   </div>
 </template>
 
@@ -8,10 +23,36 @@
 import GitTable from './components/GitTable.vue'
 
 export default {
-  name: 'app',
-  components: {
-    GitTable
-  }
+    name: 'app',
+    data() {
+        return {
+            username: '',
+            token: '',
+        };
+    },
+    mounted() {
+        this.username = localStorage.getItem('myUsername');
+        this.$refs.usernameInput.value = this.username;
+
+        this.token = localStorage.getItem('myToken');
+        this.$refs.tokenInput.value = this.token;
+    },
+    methods: {
+        setUsername() {
+            const username = this.$refs.usernameInput.value;
+            localStorage.setItem('myUsername', username);
+            this.username = username
+        },
+
+        setToken() {
+            const token = this.$refs.tokenInput.value;
+            localStorage.setItem('myToken', token);
+            this.token = token
+        },
+    },
+    components: {
+        GitTable
+    }
 }
 </script>
 
--- src/components/GitTable.vue
+++ src/components/GitTable.vue
@@ -4,6 +4,7 @@
         <tr>
             <th>#</th>
             <th>Title</th>
+            <th>Referenced tasks</th>
         </tr>
         <tr
             v-for="issue in issues"
@@ -17,16 +18,58 @@
             <td>
                 {{ issue.title }}
             </td>
+            <td v-if="!issue.timeline">
+                <button
+                    @click="loadTimeline(issue)"
+                >Load</button>
+            </td>
+            <td v-else>
+                <table>
+                    <tr>
+                        <th>#</th>
+                        <th>Title</th>
+                    </tr>
+                    <tr
+                        v-for="t in issue.timeline"
+                        :key="t.id"
+                    >
+                        <td>
+                            <a :href="t.source.issue.html_url">
+                                #{{ t.source.issue.number }}
+                            </a>
+                        </td>
+                        <td>
+                            {{ issue.title }}
+                        </td>
+
+                    </tr>
+                </table>
+            </td>
         </tr>
     </table>
   </div>
 </template>
 
 <script>
-    const octokit = require('@octokit/rest')()
+    import Vue from 'vue'
+
+    const octokit = require('@octokit/rest')(),
+        owner = '...',
+        repo = '...';
 
     export default {
         name: 'GitTable',
+
+        props: {
+            username: {
+                type: String,
+                required: true,
+            },
+            token: {
+                type: String,
+                required: true,
+            },
+        },
 
         data() {
             return {
@@ -37,14 +80,30 @@
         mounted() {
             octokit.authenticate({
                 type: 'token',
-                token: '...'
+                token: this.token,
             })
-            const q = 'repo:repo/project is:issue assignee:myUsername is:open';
+            const q = `repo:${owner}/${repo} is:issue assignee:${this.username} is:open`;
 
             octokit.search.issues({q}).then(result => {
                 this.issues = result.data.items;
             })
-
+        },
+        
+        methods: {
+            loadTimeline({ number }) {
+                octokit.issues.getEventsTimeline({
+                    owner, 
+                    repo, 
+                    number,
+                    headers: {
+                        accept: 'application/vnd.github.mockingbird-preview+json',
+                    },
+                }).then(result => {
+                    const issue = this.issues.find(issue => issue.number == number)
+                    Vue.set(issue, 'timeline', result.data.filter(d => d.event=='cross-referenced'))
+                })
+                
+            },
         },
     }
 </script>
